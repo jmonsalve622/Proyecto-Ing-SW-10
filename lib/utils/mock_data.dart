1560cfdf7ec0052fac0dbbf197e8e39e69cbdc1b
marquinhos03 <mliguempibozzano@gmail.com>
2025-11-23 02:18:23 -0300

    feat: Mock data para datos "falsos"
    - Datos predefinidos para admin (de momento).

---
+++ lib/utils/mock_data.dart
@@ -0,0 +1,9 @@
+import 'package:proyecto_ing_sw_10/utils/logic.dart';
+
+final Admin admin1 = Admin(
+  id: "a-001",
+  name: "J. Aguirre",
+  email: "jaguirre@udec.cl",
+  password: "aguirregod",
+  canDeleteReports: true,
+);
